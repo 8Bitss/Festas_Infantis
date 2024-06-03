93d1ef170ea9ccbad054fc1e76fdcdc6769bd4bb
PabloKoerich <pablokoerich@hotmail.com>
2024-06-03 15:47:10 -0300

    criando "TEMA", modulo Tema

---
+++ src/FestasInfantis.WinApp/ModuloTema/Tema.cs
@@ -0,0 +1,49 @@
+﻿using FestasInfantis.ConsoleApp.Compartilhado;
+
+namespace FestasInfantis.WinApp.ModuloTema
+{
+    public class Tema : EntidadeBase
+    {
+        public string Nome { get; set; }
+        public string Valor { get; set; }
+     // public string Intens { get; set; }
+      //public Aluguel Aluguel { get; set; }
+
+        public Tema(string nome, string valor)
+        {
+            Nome = Nome;
+            Valor = Valor;
+            //Itens = Itens;
+            //Aluguel = Aluguel;
+        }
+
+        public override void AtualizarRegistro(EntidadeBase novoRegistro)
+        {
+            Tema novoCliente = (Tema) novoRegistro;
+
+            Nome = novoCliente.Nome;
+            Valor = novoCliente.Valor;
+            //Itens = novoCliente.Itens;
+            //Aluguel = novoCliente.Aluguel;
+        }
+
+        public override List<string> Validar()
+        {
+            List<string> erros = new List<string>();
+
+            if (string.IsNullOrEmpty(Nome.Trim()))
+                erros.Add("O campo nome deve ser preenchido");
+            
+            if (string.IsNullOrEmpty(Valor.Trim()))
+                erros.Add("O campo do valor deve ser preenchido");
+
+            //if (string.IsNullOrEmpty(Itens.Trim()))
+            //    erros.Add("O campo item deve ser preenchido");
+           
+            //if (string.IsNullOrEmpty(Aluguel.Trim()))
+            //    erros.Add("O campo aluguel deve ser preenchido");
+
+            return erros;
+        }
+    }
+}
